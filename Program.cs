2b359a673b9cea9c77f7f6a1c6e4bd4d4dfeeafa
Eytan zichel <eytanzichel@gmail.com>
2024-08-08 11:33:00 +0300

    chack

--- Program.cs
+++ Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AttackServar
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            //aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            Console.WriteLine("aaaaaaaaaaa");
         }
+
+
     }
 }
